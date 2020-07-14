5086db02794f7919b000a3317a769f2fe322e929
SilaevD <silaevd@yandex.ru>
2020-07-14 15:51:49 +0300

    profile

--- src/components/profile/index.vue
+++ src/components/profile/index.vue
@@ -41,8 +41,37 @@
                 <div class="logout">
                     <button>выйти</button>
                 </div>
-
             </div>
+
+            <div class="statistics">
+                <div class="user-stat">
+                    <div class="races col">
+                        <p class="title">Гонки</p>
+                        <p class="number">5</p>
+                    </div>
+                    <div class="poles col">
+                        <p class="title">Поулы</p>
+                        <p class="number">1</p>
+                    </div>
+                    <div class="top3 col">
+                        <p class="title">Топ-3</p>
+                        <p class="number">2</p>
+                    </div>
+                    <div class="wins col">
+                        <p class="title">Победы</p>
+                        <p class="number">1</p>
+                    </div>
+                    <div class="avStart col">
+                        <p class="title">Ср. Старт</p>
+                        <p class="number">1</p>
+                    </div>
+                    <div class="avFinish col">
+                        <p class="title">Ср. Финиш</p>
+                        <p class="number">1</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
     </section>
@@ -62,6 +91,8 @@
 <style scoped lang="scss">
     a[disabled] { pointer-events: none; }
     .user {
+        display: flex;
+        flex-direction: row;
 
 
         &-info {
@@ -74,29 +105,30 @@
             display: flex;
             flex-direction: column;
             align-items: center;
+            margin-right: 50px;
             box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+
+            p {
+                width: 100%;
+                max-width: 250px;
+                display: flex;
+                justify-content: space-between;
+                margin-bottom: 5px;
+                span {
+                    &:first-child {
+                        font-weight: 500;
+                    }
+                }
+
+            }
         }
 
         &-avatar {
             width: 220px;
             height: 220px;
-            border: 4px solid var(--border-color);
+            border: 4px solid var(--accent-second-color);
             border-radius: 50%;
             margin-bottom: 30px;
-        }
-
-        p {
-            width: 100%;
-            max-width: 250px;
-            display: flex;
-            justify-content: space-between;
-            margin-bottom: 5px;
-            span {
-                &:first-child {
-                    font-weight: 500;
-                }
-            }
-
         }
 
         &-links {
@@ -142,6 +174,46 @@
         .twitch {
             &:hover {
                 color: rgb(100, 65, 165);
+            }
+        }
+
+        .statistics {
+            width: 100%;
+        }
+
+        &-stat {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            background: var(--card-background);
+            border: 3px solid var(--border-color);
+            border-radius: 4px;
+            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+            width: 100%;
+
+            .col {
+                margin-right: 1px;
+                text-align: center;
+                flex-grow: 1;
+                &:last-child {
+                    margin-right: 0;
+                }
+            }
+
+            .title {
+                color: var(--text-light-color);
+                font-weight: 500;
+                background: var(--accent-second-color);
+                padding: 7px 10px;
+                &:last-child {
+                }
+            }
+            .number {
+                padding: 7px 10px;
+                font-weight: 500;
+                /*background: var(--chips-background);*/
+                &:last-child {
+                }
             }
         }
     }
